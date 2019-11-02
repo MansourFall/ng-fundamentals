b369baae7cb7ad074d25fe7e78eafe5345462cae
Mansour Fall <fallmansour19@yahoo.fr>
2019-11-02 21:53:15 +0100

    added events-list, event-thumbnail and navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import {  EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { NavBarComponent } from './nav/navbar.component';
 
 @NgModule({
-  declarations: [
-    EventsAppComponent,
-    EventsListComponent
-  ],
   imports: [
     BrowserModule
+  ],
+  declarations: [
+    EventsAppComponent,
+    EventsListComponent,
+    EventThumbnailComponent,
+    NavBarComponent
   ],
   providers: [],
   bootstrap: [EventsAppComponent]
